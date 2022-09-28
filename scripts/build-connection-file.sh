760be35f5a597e0f0fde089a75aecfed466f78f5
Sion Smith <sion.smith@gmail.com>
2022-09-28 13:53:50 +0100

    sample platform configuration

---
+++ scripts/build-connection-file.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+
+CONFIG_FILE="
+
+sasl.mechanism=OAUTHBEARER \n
+security.protocol=SASL_PLAINTEXT \n
+sasl.jaas.config=org.apache.kafka.common.security.oauthbearer.OAuthBearerLoginModule required username=\"$MDS_USERNAME\" password=\"$MDS_PASSWORD\" metadataServerUrls=\"$MDS_URL\"; \n
+sasl.login.callback.handler.class=io.confluent.kafka.clients.plugins.auth.token.TokenUserLoginCallbackHandler \n
+topology.builder.access.control.class=com.purbon.kafka.topology.roles.RBACProvider \n
+topology.builder.mds.server=$MDS_URL \n
+topology.builder.mds.user=$MDS_USERNAME \n
+topology.builder.mds.password=$MDS_PASSWORD \n
+topology.builder.mds.kafka.cluster.id=$KAFKA_CLUSER_ID \n
+schema.registry.url=$SCHEMA_REGISTRY_URL \n
+schema.registry.basic.auth.user.info=$MDS_USERNAME:$MDS_PASSWORD \n
+basic.auth.credentials.source=USER_INFO \n
+topology.builder.mds.schema.registry.cluster.id=schema-registry \n
+topology.builder.mds.kafka.connect.cluster.id=connect-cluster \n
+topology.builder.mds.ksqldb.cluster.id=ksql-cluster \n
+allow.delete.topics=$ALLOW_TOPIC_DELETE \n
+topology.topic.prefix.separator=-
+"
+
+echo -e $CONFIG_FILE
